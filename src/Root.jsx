ae4cf9cc0cef9b595aec94d33c68f0812c213b80
Maximiliano Paci <pacimaximiliano@gmail.com>
2021-10-28 10:07:58 -0300

    creating "/" main route for the component App

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import App from './App'
 
 export default function () {
   return (
-    <Router />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </Router>
   )
 }

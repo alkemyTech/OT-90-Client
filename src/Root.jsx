e9980b0024bff1a3cb0def5c4223fa9898fc78a3
Maximiliano Paci <pacimaximiliano@gmail.com>
2021-10-28 10:13:54 -0300

    adding Root component to index file

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import App from './App'
 
-export default function () {
+export default function Root() {
   return (
     <Router>
       <Switch>

aa598aaea53b042c191b6c5ceea99ef05821a6cf
benjaminPla <benjaminpla.dev@gmail.com>
2021-10-28 10:32:32 -0300

    add Header component, delete examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
-import React, { useState } from 'react'
-import ButtonComponent from './Component/Button'
-import AlertComponent from './Component/Alert';
+import React from 'react'
+import Header from './Component/Header.jsx'
 
 import './App.css'
 import './static/styles/Alert.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const initialAlertState = { status: false, title: '', content: '' }
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  const [showAlert, setShowAlert] = useState(initialAlertState)
-
-  const openAlert = () => {
-    setShowAlert({
-      status: true,
-      title: 'Message Alert',
-      content: 'Content of alert component',
-    })
-  }
-
-  const closeAlert = () => {
-    setShowAlert(initialAlertState)
-  }
-
   return (
     <div className="App">
-      <div className="alert-custom">
-        <AlertComponent show={showAlert.status} action={() => closeAlert()} {...showAlert} />
-      </div>
-      <header className="App-header">
-        <ButtonComponent title="Test Button" onClick={() => openAlert()} />
-      </header>
+      <Header />
     </div>
   )
 }
---
+++ src/Component/Header.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Header() {
+  const [publicData, setPublicData] = useState(null)
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then((res) => res.json())
+      .then((data) => setPublicData(data))
+      .catch((error) => console.log(error))
+  }, [])
+  console.log(publicData)
+  return (
+    <>
+      {publicData && (
+        <>
+          {/* this would be the logo */}
+          <img src={publicData.sprites.front_default} alt={publicData.name} />
+          {/* this would be the items */}
+          <ul>
+            {publicData.abilities.map(({ ability }, index) => {
+              return <li key={index}>{ability.name}</li>
+            })}
+          </ul>
+        </>
+      )}
+    </>
+  )
+}

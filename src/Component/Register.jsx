a8d762872dba4f8427979ee747ae5ae54e64439b
FranMerchan <merchanf6@gmail.com>
2021-10-29 11:04:09 -0300

    Installed Formik, created the registry path and the Register element

---
+++ src/Component/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => (
+  <>
+    <div className="formContainer vh-100 d-flex align-items-center justify-content-center">
+      <h1 className="bg-dark">Hola registro</h1>
+    </div>
+  </>
+)
+
+export default Register
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import App from '../App'
+import Register from '../Component/Register'
 
 export default function Root() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={App} />
-        <Route path="/nosotros" component={() => '"Nostros" Screen under construction'} />
+        <Route
+          path="/nosotros"
+          component={() => '"Nostros" Screen under construction'}
+        />
+        <Route path="/register" component={Register} />
       </Switch>
     </Router>
   )

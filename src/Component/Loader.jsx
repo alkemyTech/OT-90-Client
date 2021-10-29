c9ffc488fa10a101a40b99a56e6cc0f166a0697f
andresmonsa <monsalbefotografia@gmail.com>
2021-10-29 14:20:27 -0300

    change the way the loader is displayed

--- src/Component/Loader.jsx
+++ src/Component/Loader.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import Spinner from 'react-loader-spinner'
-
+import '../static/styles/Loader.css'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 
-const Loader = ({ time = 3000, height = 100, width = 100 }) => (
+const Loader = ({
+  height = 500, width = 500, visible = false,
+}) => (
   <Spinner
-    type="Puff"
+    type="Circles"
     color="#00BFFF"
     height={height}
     width={width}
-    timeout={time}
+    visible={visible}
+    className="loader"
   />
 )
 

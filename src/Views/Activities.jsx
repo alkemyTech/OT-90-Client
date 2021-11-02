3ed41363137b987da2e31fa4878bce472e7544e6
Maximiliano Paci <maximiliano.paci@sarsy-srl.com>
2021-11-02 10:35:54 -0300

    adding header and changing data example names

--- src/Views/Activities.jsx
+++ src/Views/Activities.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Footer from '../Component/Footer';
 import Table from '../Component/Table'
+import Header from '../Component/Header';
 
 const colNames = ['Nombre', 'Acciones']
-const data = [{ name: 'pep' }, { name: 'pep' }, { name: 'pep' }]
+const data = [{ name: 'Taller' }, { name: 'Paseos' }, { name: 'Taller' }, { name: 'Taller' }]
 
 function Activities() {
   return (
     <div className="App">
+      <Header />
       <div
         className="container"
         style={{

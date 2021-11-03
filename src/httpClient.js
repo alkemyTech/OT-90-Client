fd2063a4a65a5f52f1cd0f78b5ee508e6f65412f
Benjamin Pla <benjaminpla.dev@gmail.com>
2021-11-03 12:41:02 -0300

    Update httpClient.js

--- src/httpClient.js
+++ src/httpClient.js
@@ -2,8 +2,6 @@ import axios from 'axios'
 
 export default async function request(method, url) {
   const userData = JSON.parse(localStorage.getItem('user-data')) || null
-  if (!userData) return { error: 'no userData' }
-  if (!userData.token) return { error: 'no token' }
   const token = `Bearer  ${userData.token}`
   const response = await axios({ method, url, headers: { Authorization: token } })
     .catch((error) => error.response)

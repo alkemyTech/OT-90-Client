ee25ee687ac61fb9d15b03c9780a4532a6d70cdd
Benjamin Pla <benjaminpla.dev@gmail.com>
2021-11-05 10:00:18 -0300

    Update httpClient.js

--- src/httpClient.js
+++ src/httpClient.js
@@ -1,9 +1,19 @@
 import axios from 'axios'
+import dotenv from 'dotenv'
 
-export default async function request(method, url) {
+dotenv.config()
+
+export default async function sendRequest(method, relativeUrl, data) {
+  let token = null
   const userData = JSON.parse(localStorage.getItem('user-data')) || null
-  const token = `Bearer  ${userData.token}`
-  const response = await axios({ method, url, headers: { Authorization: token } })
+  if (userData === !null) token = `Bearer  ${userData.token}`
+  const baseUrl = process.env.REACT_APP_NODE_ENV === 'production'
+    ? process.env.REACT_APP_BASE_URL_PRODUCTION
+    : process.env.REACT_APP_BASE_URL_LOCAL
+  const url = baseUrl + relativeUrl
+  const response = await axios({
+    method, url, data, headers: { Authorization: token },
+  })
     .catch((error) => error.response)
   return response
 }

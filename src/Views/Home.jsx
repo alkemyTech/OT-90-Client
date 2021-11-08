d4202b4aac5cdbd0fcaf7fce102e57eb64bc60dd
Marcelo Nieva <marcelonieva@gmail.com>
2021-11-08 13:22:40 -0300

    Add Home component

---
+++ src/Views/Home.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useReducer, useState } from 'react';
+import PropTypes from 'prop-types'
+import {
+  Card, Row, Col, Container,
+} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import AlertComponent from '../Component/Alert';
+import Loader from '../Component/Loader';
+import Slider from '../Component/Slider';
+
+const Home = ({ news }) => (
+  <Container>
+    <Slider />
+    <h1 className="my-3">Bienvenido a Somos Mas</h1>
+    <h2>Ultimas Novedades</h2>
+    <Row xs={1} md={2} lg={4} className="g-4">
+      {news.map(({ name, image, id }) => (
+        <Col key={id}>
+          <Card>
+            <Link className="text-decoration-none text-dark" to={`/novedades/${id}`}>
+              <Card.Img variant="top" src={image} />
+              <Card.Body>
+                <Card.Title className="text-decoration-none">{name}</Card.Title>
+              </Card.Body>
+            </Link>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  </Container>
+)
+
+const HomeContainer = () => {
+  const initialState = {
+    data: {},
+    isLoading: true,
+    error: null,
+  }
+
+  const reducer = (state, action) => {
+    const { type, payload = {} } = action
+    switch (type) {
+      case 'GET_DATA':
+        return {
+          isLoading: true,
+          data: {},
+          error: null,
+        };
+      case 'GET_DATA_OK':
+        return {
+          isLoading: false,
+          data: payload,
+          error: null,
+        };
+      case 'ERROR':
+        return {
+          isLoading: false,
+          error: payload,
+          data: {},
+        };
+      default:
+        return state
+    }
+  }
+  const [{ error, data, isLoading }, dispatch] = useReducer(reducer, initialState)
+
+  const [toggle, setToggle] = useState(false)
+
+  const alertAction = () => {
+    dispatch({ type: 'GET_DATA' })
+    setToggle(!toggle)
+  }
+
+  useEffect(() => {
+    const getNews = async () => {
+      try {
+        const fetchNews = await fetch('http://localhost:3001/news')
+        const newsJson = await fetchNews.json()
+        if (!fetchNews.ok) {
+          dispatch({ type: 'ERROR', payload: newsJson })
+          return
+        }
+        dispatch({ type: 'GET_DATA_OK', payload: newsJson })
+      } catch (e) {
+        dispatch({ type: 'ERROR', payload: e })
+      }
+    }
+    getNews()
+  }, [toggle])
+
+  if (isLoading) {
+    return <Loader visible />
+  }
+  return (
+    error
+      ? <AlertComponent show={!isLoading} title="Error obteniendo novedadades" variant="warning" action={alertAction} />
+      : <Home news={data} />
+  )
+}
+
+Home.propTypes = {
+  news: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
+
+export default HomeContainer

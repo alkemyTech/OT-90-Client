849178ce6ff97cc090bb3ada13657379413aab83
Marcelo Nieva <marcelonieva@gmail.com>
2021-11-08 13:21:26 -0300

    set home route

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import App from '../App'
+// import App from '../App'
 import Register from '../Component/Register'
+import Home from '../Views/Home'
 import Login from '../Views/Login'
 import Backoffice from './Backoffice'
 
@@ -9,7 +10,8 @@ export default function Root() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={App} />
+        {/* <Route exact path="/" component={App} /> */}
+        <Route exact path="/" component={Home} />
         <Route path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <Route path="/nosotros" component={() => '"Nostros" Screen under construction'} />

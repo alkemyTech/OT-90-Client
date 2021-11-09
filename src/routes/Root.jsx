4db80845055ad827a81ea00fc9f2a7dd996312f3
Carlos Marcelo Nieva <72712805+marcelonieva7@users.noreply.github.com>
2021-11-08 21:43:39 -0300

    OT90-665 (#17)
    * set news datail rout
    
    * add news detail view
    
    * fix route
    
    * add httprequest service
    
    Co-authored-by: Marcelo Nieva <marcelonieva@gmail.com>

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import App from '../App'
 import Register from '../Component/Register'
 import Login from '../Views/Login'
+import NewsDetail from '../Views/NewsDetail'
 import Backoffice from './Backoffice'
 
 export default function Root() {
@@ -12,6 +13,7 @@ export default function Root() {
         <Route exact path="/" component={App} />
         <Route path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/novedades/:id" component={NewsDetail} />
         <Route path="/nosotros" component={() => '"Nostros" Screen under construction'} />
         <Route path="/backoffice" component={Backoffice} />
       </Switch>

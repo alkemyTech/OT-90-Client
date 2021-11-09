4ea2f100f1da57a23f5ee3c14a19b015b0dcac37
benjaminPla <benjaminpla.dev@gmail.com>
2021-11-09 11:02:51 -0300

    add role to httpClient

--- src/httpClient.js
+++ src/httpClient.js
@@ -6,13 +6,14 @@ dotenv.config()
 export default async function sendRequest(method, relativeUrl, data) {
   let token = null
   const userData = JSON.parse(localStorage.getItem('user-data')) || null
+  const role = userData.role
   if (userData !== null) token = `Bearer ${userData.authorization}`
   const baseUrl = process.env.REACT_APP_NODE_ENV === 'production'
     ? process.env.REACT_APP_BASE_URL_PRODUCTION
     : process.env.REACT_APP_BASE_URL_LOCAL
   const url = baseUrl + relativeUrl
   const response = await axios({
-    method, url, data, headers: { Authorization: token },
+    method, url, data, headers: { Authorization: token, roleId: role },
   })
     .catch((error) => { throw error.message })
   return response

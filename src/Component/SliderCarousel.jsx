16a8845f16c0658242d951b58a2acef83e1066e2
Carlos Marcelo Nieva <72712805+marcelonieva7@users.noreply.github.com>
2021-11-08 21:59:54 -0300

    OT90-654 (#21)
    * set home route
    
    * Add Home component
    
    * fixes Slider
    
    * add httpRequest service
    
    * add limit
    
    Co-authored-by: Marcelo Nieva <marcelonieva@gmail.com>

--- src/Component/SliderCarousel.jsx
+++ src/Component/SliderCarousel.jsx
@@ -2,23 +2,24 @@ import '../static/styles/SliderCarousel.css'
 
 import Carousel from 'react-bootstrap/Carousel'
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const SliderCarousel = (props) => {
-  return (
-  <>
+const SliderCarousel = ({ fetchSlider }) => (
   <Carousel className="carouselContainer">
-    {props.fetchSlider.length > 0  ? (
-      props.fetchSlider.map( (slide, i) => {
-        return  <Carousel.Item key={i}>
-          <img className="d-block w-100" src={slide.imageURL} />
-            <Carousel.Caption className="carouselCaption">
-              <p>{slide.text}</p>
-            </Carousel.Caption>
-        </Carousel.Item>                    
-    }) ) : null } 
+    {fetchSlider.length > 0 ? (
+      fetchSlider.map(({ text, imageURL }) => (
+        <Carousel.Item className="carouselItem" key={imageURL}>
+          <img className="d-block w-100" alt="" src={imageURL} />
+          <Carousel.Caption className="carouselCaption">
+            <p>{text}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))) : null }
   </Carousel>
-  </>
-    )
-}
+)
 
 export default SliderCarousel
+
+SliderCarousel.propTypes = {
+  fetchSlider: PropTypes.arrayOf(PropTypes.object).isRequired,
+}

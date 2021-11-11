45c6790cae3ab3ee5db981f1e4850f897649f751
Maximiliano Paci <pacimaximiliano@gmail.com>
2021-11-11 10:55:54 -0300

    modifying backoffice component

--- src/Component/Backoffice.jsx
+++ src/Component/Backoffice.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import PropTypes from 'prop-types'
 import Button from 'react-bootstrap/Button'
 import '../static/styles/Backoffice.css'
+import { useHistory } from 'react-router-dom'
+import { selectUser } from '../app/userSlice'
 
 // localStorage.setItem('user-data', JSON.stringify(
 // { username: 'Ben', token: null, role: 'admin' }))
@@ -9,24 +13,27 @@ import '../static/styles/Backoffice.css'
 const application = {
   sucess: true,
   body: [
-    { app: 'Clientes', permission: ['admin'] },
-    { app: 'Reportes', permission: ['admin'] },
-    { app: 'Fichadas', permission: ['admin'] },
-    { app: 'Update', permission: ['admin'] },
-    { app: 'Tablero', permission: ['admin'] },
-    { app: 'Backup', permission: ['admin'] },
-    { app: 'Otras aplicaciones', permission: ['admin'] },
-    { app: 'Editar perfil', permission: ['user', 'admin'] }],
+    { app: 'Novedades', permission: 'admin', route: '/news' },
+    { app: 'Actividades', permission: 'admin', route: '/activities' },
+    { app: 'Categorias', permission: 'admin', route: '/categories' },
+    { app: 'Contactos', permission: 'admin', route: '/contacts' },
+    { app: 'Testimonios', permission: 'admin', route: '/testimonials' },
+    { app: 'Usuarios', permission: 'admin', route: '/users' }],
 }
 
-export default function Backoffice() {
-  const { role } = JSON.parse(localStorage.getItem('user-data'))
+export default function Backoffice({ path }) {
+  const history = useHistory()
+  const { role } = useSelector(selectUser).user
   const appsFiltered = role === 'admin'
     ? application.body
     : application.body.filter((app) => app.permission.includes(role))
   return (
     <div className="grid">
-      {appsFiltered.map((app) => (<Button size="lg" key={app.app}>{app.app}</Button>))}
+      {appsFiltered.map((app) => (<Button size="lg" key={app.app} onClick={() => history.push(`${path}${app.route}`)}>{app.app}</Button>))}
     </div>
   )
+}
+
+Backoffice.propTypes = {
+  path: PropTypes.string.isRequired,
 }

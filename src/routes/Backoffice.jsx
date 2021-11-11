594f95276d2600b9780bb39a893be07d00d2c280
maxipaci <62407328+maxipaci@users.noreply.github.com>
2021-11-11 19:10:52 -0300

    Ot90 678 (#33)
    * adding conditional route
    
    * backoffice route protection
    
    * protecting backoffice options for admin user
    
    * adding backoffice Main View
    
    * userData formating
    
    * basing protected routing in redux state and adding backoffice main view
    
    * modifying backoffice component
    
    * adding profile update option to backoffice
    
    * adapting redux changes

--- src/routes/Backoffice.jsx
+++ src/routes/Backoffice.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
 import Categories from '../Views/Backoffice/Catogeries'
 import Contacts from '../Views/Backoffice/Contacts'
@@ -7,20 +8,24 @@ import Testimonials from '../Views/Testimonials'
 import Activities from '../Views/Backoffice/Activities'
 import News from '../Views/Backoffice/News'
 import Users from '../Views/Backoffice/Users'
+import Conditional from './ConditionalRoute'
+import { selectUser } from '../app/userSlice'
+import BackofficeMain from '../Views/Backoffice/Main'
 
 const Backoffice = (props) => {
+  const isAdmin = useSelector(selectUser).role === 'admin'
   const { match } = props
   const { path } = match
   return (
     <Router>
       <Switch>
-        <Route exact path={path} component={() => 'Main backoffice route'} />
-        <Route path={`${path}/activities`} component={Activities} />
-        <Route path={`${path}/categories`} component={Categories} />
-        <Route path={`${path}/contacts`} component={Contacts} />
-        <Route path={`${path}/testimonials`} component={Testimonials} />
-        <Route exact path={`${path}/news`} component={News} />
-        <Route exact path={`${path}/users`} component={Users} />
+        <Route exact path={path} render={() => <BackofficeMain path={path} />} />
+        <Conditional path={`${path}/activities`} component={Activities} conditionToOpen={isAdmin} pathRedirect={path} />
+        <Conditional path={`${path}/categories`} component={Categories} conditionToOpen={isAdmin} pathRedirect={path} />
+        <Conditional path={`${path}/contacts`} component={Contacts} conditionToOpen={isAdmin} pathRedirect={path} />
+        <Conditional path={`${path}/testimonials`} component={Testimonials} conditionToOpen={isAdmin} pathRedirect={path} />
+        <Conditional exact path={`${path}/news`} component={News} conditionToOpen={isAdmin} pathRedirect={path} />
+        <Conditional exact path={`${path}/users`} component={Users} conditionToOpen={isAdmin} pathRedirect={path} />
       </Switch>
     </Router>
   )

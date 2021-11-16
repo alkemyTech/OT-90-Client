82841a7fa3922c64e3149c0e17ab56532f4576bd
andresmonsa <monsalbefotografia@gmail.com>
2021-11-16 18:30:02 -0300

    animation integred

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,5 +1,11 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
 import Backoffice from './Backoffice'
 import Conditional from './ConditionalRoute'
 import Contact from '../Views/Contact'
@@ -9,17 +15,24 @@ import Login from '../Views/Login'
 import NewsDetail from '../Views/NewsDetail'
 import ActivitiesDetail from '../Views/ActivitiesDetails'
 import News from '../Views/News'
-import React from 'react'
 import Register from '../Component/Register'
 import { selectUser } from '../app/userSlice'
 // import App from '../App'
-import { useSelector } from 'react-redux'
 
 export default function Root() {
   const isLogged = useSelector(selectUser).isAuthenticated
+  console.log(isLogged, 'LOGGED')
   return (
     <Router>
       <Route path="/" component={Header} />
+      {/* <Route render={({ location }) => (
+        <TransitionGroup>
+          <CSSTransition
+            key={location.key}
+            timeout={450}
+            classNames="fade"
+          > */}
+      {/* <Switch location={location}> */}
       <Switch>
         {/* <Route exact path="/" component={App} /> */}
         <Route exact path="/" component={Home} />
@@ -32,6 +45,10 @@ export default function Root() {
         <Route exact path="/contacto" component={Contact} />
         <Conditional conditionToOpen={isLogged} component={Backoffice} pathRedirect="/" path="/backoffice" />
       </Switch>
+      {/* </CSSTransition>
+        </TransitionGroup>
+      )}
+      /> */}
     </Router>
   )
 }

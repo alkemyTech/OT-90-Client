6ed28a613bda76b1d2a905b5b7d1f539869364f9
Dario Velez Roman <dvelezroman@gmail.com>
2021-11-18 16:31:41 -0300

    fix http client

--- src/httpClient.js
+++ src/httpClient.js
@@ -1,14 +1,18 @@
 import axios from 'axios'
 
+const baseUrl = process.env.NODE_ENV === 'development'
+  ? process.env.REACT_APP_BASE_URL_LOCAL
+  : process.env.REACT_APP_BASE_URL_PRODUCTION
+
 export default async function sendRequest(method, relativeUrl, data) {
+  const headers = {}
   const userData = JSON.parse(localStorage.getItem('user-data'))
-  const Authorization = userData ? `Bearer ${userData.token}` : null
-  const baseUrl = process.env.NODE_ENV === 'development'
-    ? process.env.REACT_APP_BASE_URL_LOCAL
-    : process.env.REACT_APP_BASE_URL_PRODUCTION
+  if (userData && userData.token) {
+    headers.Authorization = `Bearer ${userData.token}`
+  }
   const url = baseUrl + relativeUrl
   const response = await axios({
-    method, url, data, headers: { Authorization },
+    method, url, headers, data,
   })
     .catch((error) => {
       throw error.message

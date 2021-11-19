fbe2d4b3179b145dcc095676f8318f09ba40d74e
Maximiliano Paci <pacimaximiliano@gmail.com>
2021-11-18 23:30:18 -0300

    fixing backoffice routes

--- src/Views/Backoffice/Main.jsx
+++ src/Views/Backoffice/Main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Header from '../../Component/Header'
 import Backoffice from '../../Component/Backoffice'
 import '../../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,7 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function BackofficeMain({ path }) {
   return (
     <div className="App">
-      <Header />
       <Backoffice path={path} />
     </div>
   )

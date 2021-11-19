800b5f38f0b5713aa7ccf4ed0d4ccfde04b777f2
Marcelo Nieva <marcelonieva@gmail.com>
2021-11-19 16:41:35 -0300

    add svg wave background

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -47,6 +47,15 @@ export default function Root() {
   return (
     <Router>
       <Header />
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none" className="svg position-absolute d-none d-lg-block" style={{ height: '890px', width: '100%', 'z-index': '-10' }}>
+        <defs>
+          <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
+            <stop stopColor="hsl(216, 55%, 95.1%)" offset="0%" />
+            <stop stopColor="hsl(216, 55%, 85.1%)" offset="100%" />
+          </linearGradient>
+        </defs>
+        <path fill="url(#sw-gradient-0)" d="M 0.351 264.418 C 0.351 264.418 33.396 268.165 47.112 270.128 C 265.033 301.319 477.487 325.608 614.827 237.124 C 713.575 173.504 692.613 144.116 805.776 87.876 C 942.649 19.853 1317.845 20.149 1440.003 23.965 C 1466.069 24.779 1440.135 24.024 1440.135 24.024 L 1440 0 L 1360 0 C 1280 0 1120 0 960 0 C 800 0 640 0 480 0 C 320 0 160 0 80 0 L 0 0 L 0.351 264.418 Z" />
+      </svg>
       <Switch>
         {/* <Route exact path="/" component={App} /> */}
         <Route exact path="/" component={Home} />

73d975421058675f08c6e180f76d091c6f6463af
Carlos Marcelo Nieva <72712805+marcelonieva7@users.noreply.github.com>
2021-11-24 18:21:55 -0300

    OT90-742 (#67)
    * redirect to news list after save changes
    
    * redirect to news list if id not found
    
    * add button to create news
    
    * add view News Create
    
    * add route
    
    * fix issue getting error
    
    Co-authored-by: Marcelo Nieva <marcelonieva@gmail.com>

--- src/Views/Backoffice/News.jsx
+++ src/Views/Backoffice/News.jsx
@@ -1,23 +1,25 @@
 import React, { useEffect, useReducer, useState } from 'react';
-import AlertComponent from '../../Component/Alert';
+import { Button, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import Loader from '../../Component/Loader';
 import Table from '../../Component/Table'
 import sendRequest from '../../httpClient'
 
 const NewsContainer = () => {
   const initialState = {
-    data: {},
+    data: [],
     isLoading: true,
     error: null,
   }
 
   const reducer = (state, action) => {
-    const { type, payload = {} } = action
+    const { type, payload } = action
     switch (type) {
       case 'GET_DATA':
         return {
           isLoading: true,
-          data: {},
+          data: [],
           error: null,
         };
       case 'GET_DATA_OK':
@@ -30,7 +32,7 @@ const NewsContainer = () => {
         return {
           isLoading: false,
           error: payload,
-          data: {},
+          data: [],
         };
       case 'DELETE_OK':
         return {
@@ -42,14 +44,9 @@ const NewsContainer = () => {
         return state
     }
   }
-  const [{ error, data, isLoading }, dispatch] = useReducer(reducer, initialState)
+  const [{ data, isLoading }, dispatch] = useReducer(reducer, initialState)
 
   const [toggle, setToggle] = useState(false)
-
-  const alertAction = () => {
-    dispatch({ type: 'GET_DATA' })
-    setToggle(!toggle)
-  }
 
   const deleteNew = async (id) => {
     try {
@@ -67,6 +64,18 @@ const NewsContainer = () => {
         dispatch({ type: 'GET_DATA_OK', payload: news })
       } catch (e) {
         dispatch({ type: 'ERROR', payload: e })
+        const { isConfirmed } = await Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Ocurrio un error obteniendo la información, intenta nuevamente.',
+          confirmButtonText: 'Reintentar',
+          allowOutsideClick: false,
+          allowEscapeKey: false,
+        })
+        if (isConfirmed) {
+          dispatch({ type: 'GET_DATA' })
+          setToggle(!toggle)
+        }
       }
     }
     getNews()
@@ -74,13 +83,11 @@ const NewsContainer = () => {
 
   const headers = ['name', 'image', 'createdAt']
 
-  if (isLoading) {
-    return <Loader visible />
-  }
-  return (
-    error
-      ? <AlertComponent show={!isLoading} title="Error obteniendo novedadades" variant="warning" action={alertAction} />
-      : <Table title="Novedades" headers={headers} data={data} onDelete={deleteNew} />
+  return isLoading ? <Loader visible /> : (
+    <Container>
+      <Button as={Link} size="lg" variant="info" className="mt-4 me-2 position-absolute end-0" to="/backoffice/news/create">Crear Novedad</Button>
+      <Table title="Novedades" headers={headers} data={data} onDelete={deleteNew} />
+    </Container>
   )
 }
 

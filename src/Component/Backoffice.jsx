4fb16ac159305f098c64d939dcf1e5810a08f590
Carlos Marcelo Nieva <72712805+marcelonieva7@users.noreply.github.com>
2021-11-26 17:04:08 -0300

    OT90-743 (#76)
    * add icons
    
    * change styles
    
    * fix edit contacts route
    
    Co-authored-by: Marcelo Nieva <marcelonieva@gmail.com>

--- src/Component/Backoffice.jsx
+++ src/Component/Backoffice.jsx
@@ -1,27 +1,48 @@
-import '../static/styles/Backoffice.css'
-
-import Button from 'react-bootstrap/Button'
-import PropTypes from 'prop-types'
 import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  FaNewspaper, FaComment, FaUserEdit, FaUsers,
+} from 'react-icons/fa';
+import { BiCategory } from 'react-icons/bi';
+import { AiFillPlayCircle } from 'react-icons/ai';
+import { RiContactsFill } from 'react-icons/ri';
+import { VscOrganization } from 'react-icons/vsc';
+import { IconContext } from 'react-icons';
+import {
+  Container, Button, Row, Col,
+} from 'react-bootstrap'
 import { useHistory } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { selectUser } from '../app/userSlice'
+import '../static/styles/Backoffice.css'
 
-// localStorage.setItem('user-data', JSON.stringify(
-// { username: 'Ben', token: null, role: 'admin' }))
-
-// response from srv:
 const application = {
   sucess: true,
   body: [
-    { app: 'Novedades', permission: 'admin', route: '/news' },
-    { app: 'Categorias', permission: 'admin', route: '/allcategories' },
-    { app: 'Actividades', permission: 'admin', route: '/allactivities' },
-    { app: 'Contactos', permission: 'admin', route: '/contacts' },
-    { app: 'Testimonios', permission: 'admin', route: '/alltestimonials' },
-    { app: 'Usuarios', permission: 'admin', route: '/users' },
-    { app: 'EditOrganization', permission: 'admin', route: '/editorganization/1' },
-    { app: 'Editar Perfil', permission: 'standard', route: '/profile' }],
+    {
+      app: 'Novedades', permission: 'admin', route: '/news', icon: <FaNewspaper />,
+    },
+    {
+      app: 'Actividades', permission: 'admin', route: '/allactivities', icon: <AiFillPlayCircle />,
+    },
+    {
+      app: 'Categorias', permission: 'admin', route: '/allcategories', icon: <BiCategory />,
+    },
+    {
+      app: 'Contactos', permission: 'admin', route: '/contacts', icon: <RiContactsFill />,
+    },
+    {
+      app: 'Testimonios', permission: 'admin', route: '/alltestimonials', icon: <FaComment />,
+    },
+    {
+      app: 'Usuarios', permission: 'admin', route: '/users', icon: <FaUsers />,
+    },
+    {
+      app: 'Editar Organization', permission: 'admin', route: '/editorganization/1', icon: <VscOrganization />,
+    },
+    {
+      app: 'Editar Perfil', permission: 'standard', route: '/profile', icon: <FaUserEdit />,
+    }],
 }
 
 export default function Backoffice({ path }) {
@@ -31,9 +52,21 @@ export default function Backoffice({ path }) {
     ? application.body
     : application.body.filter((app) => app.permission.includes(role.toLowerCase().trim()))
   return (
-    <div className="grid">
-      {appsFiltered.map((app) => (<Button size="lg" key={app.app} onClick={() => history.push(`${path}${app.route}`)}>{app.app}</Button>))}
-    </div>
+    <Container className="backoffice_container">
+      <Row className="row_button my-5">
+        <IconContext.Provider value={{ color: 'white', size: '3em' }}>
+          {appsFiltered.map((app) => (
+            <Col xs={6} md={4} key={app.app}>
+              <Button className="w-100 py-md-5 px-2" size="lg" onClick={() => history.push(`${path}${app.route}`)}>
+                {app.icon}
+                <br />
+                {app.app}
+              </Button>
+            </Col>
+          ))}
+        </IconContext.Provider>
+      </Row>
+    </Container>
   )
 }
 

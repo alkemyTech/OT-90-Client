c603484b80140522e6111c1bf047b1ae12b743f2
verteramofederico <verteramo.federico@gmail.com>
2021-11-26 11:02:07 -0300

    adding photos to carousel

--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -3,16 +3,16 @@ import SliderCarousel from './SliderCarousel';
 
 const fetchSlider = [
   {
-    imageURL: 'https://static2.elcorreo.com/www/pre2017/multimedia/noticias/201703/31/media/cortadas/prueba-01-ketF-U2131796188083t-575x323@El%20Correo.jpg',
-    text: 'Nulla vitae elit libero, a pharetra augue mollis interdum.',
+    imageURL: 'images/manos10.jpg',
+    text: 'Somos mas',
   },
   {
-    imageURL: 'https://i.blogs.es/a19bfc/testing/450_1000.jpg',
-    text: 'lorem impsum oksokko',
+    imageURL: 'images/foto10.jpg',
+    text: 'Somos mas',
   },
   {
-    imageURL: 'https://www.uba.ar/internacionales/archivos/TEST.jpg',
-    text: 'la vie est una chose important',
+    imageURL: 'images/foto11.jpg',
+    text: 'Somos mas',
   },
 ]
 

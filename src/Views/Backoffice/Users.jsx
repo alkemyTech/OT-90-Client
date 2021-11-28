8519cf7284c5a6b6581e6ef585a176d0bda813fa
AndresMonsa <monsalbefotografia@gmail.com>
2021-11-28 14:20:49 -0300

    Add category (#77)
    * pagination logic ok, without style
    
    * pagination btn now fit in screen
    
    * testimony title
    
    * add category
    
    * fixed useEffect bug

--- src/Views/Backoffice/Users.jsx
+++ src/Views/Backoffice/Users.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react'
 import Table from '../../Component/Table'
 import sendRequest from '../../httpClient'
 import HttpActionEnum from '../../enums/HttpActionEnum'
+
 // this line below is just for testing only
 // eslint-disable-next-line max-len
-// localStorage.setItem('user-data', JSON.stringify({ username: 'Ben', token: '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', role: '1' }))
 
 export default function Users() {
   const [users, setUsers] = useState([])
@@ -15,6 +15,6 @@ export default function Users() {
   }, [])
   const headers = ['firstName', 'lastName', 'email', 'image']
   return (
-    <><Table headers={headers} data={users} title="Users" /></>
+    <Table headers={headers} data={users} title="Users" />
   )
 }
